bf5198b5d92a705ef69c1caa8f4bdeb119b07695
Ivan <khrustalev16@bk,ru>
2023-01-13 19:43:09 +0300

    13.01(10)

--- app/Console/Commands/DeviceCron.php
+++ app/Console/Commands/DeviceCron.php
@@ -54,7 +54,7 @@ class DeviceCron extends Command
             $result[] = [$ObjName => [$arrNumber[$key] => $arrNames[$key]]] ;
         }
         If(isset($result)){
-        Mail::to('khrustalev16@gmail.com')->send(new MailCron( $result));
+        Mail::to('ergogaz@gmail.com')->send(new MailCron( $result));
               return Log::info(1);
          }
 }
