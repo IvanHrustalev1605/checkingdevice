5db33f8da5c35367b5fa6721ad5f259f21ea4844
Ivan <khrustalev16@bk,ru>
2023-01-15 14:04:49 +0300

    15/01

--- resources/views/allusers/index.blade.php
+++ resources/views/allusers/index.blade.php
@@ -1,16 +1,15 @@
 @extends('layout')
 @section('content')
 
-<div class="row m-4 justify-content-evenly">
+<div class="row justify-content-evenly">
 @foreach($users as $user)
-    <div class="col-sm-4 m-2">
-        <div class="card text-center" style="width: 10rem;">
+    <div class="col-sm-3">
+    <div class="card text-dark bg-light mb-3" style="max-width: 10rem;">
         <a href="{{route('userIndex', $user->uid)}}">
             <img src="/public/storage/{!!$user->avatar!!}" class="card-img-top" alt="...">
+            <div class="card-header">{{$user->post}}</div>
                 <div class="card-body">
-                
                     <h5 class="card-title">{{$user->name}} {{$user->surname}}</h5>
-                    <p class="card-text">{{$user->post}}</p>
                     <p class="card-text">{{$user->mobile}}</p>
                     </a>
                 </div>
